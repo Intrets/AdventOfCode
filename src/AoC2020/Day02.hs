e26e352bcbb43bbeebfb91d7b69fdab40b10f15a
Intrets <geen.inspiratie824@gmail.com>
2020-12-02 13:34:37 +0100

    day 2

--- src/AoC2020/Day02.hs
+++ src/AoC2020/Day02.hs
@@ -1,6 +1,7 @@
 module AoC2020.Day02 where
 
 import           Text.ParserCombinators.Parsec
+import           Data.Function
 
 parser :: (Int -> Int -> Char -> String -> Bool) -> Parser Int
 parser f = length . filter id <$> many
@@ -21,7 +22,10 @@ main = do
   inputData <- readFile "src/AoC2020/Day02.txt"
 
   let part1 a b c = ((&&) <$> (>= a) <*> (<= b)) . length . filter (== c)
-  let part2 a b c = (/=) <$> (== c) . (!! (a - 1)) <*> (== c) . (!! (b - 1))
+  let part2 a b c = ((/=) `on` (== c)) <$> (!! (a - 1)) <*> (!! (b - 1))
 
   putStr "part one: " >> print (parse (parser part1) "" inputData)
   putStr "part two: " >> print (parse (parser part2) "" inputData)
+
+
+
