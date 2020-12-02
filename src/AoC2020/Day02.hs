cd2cd830ff4be85004225cbf0a91c98f5cead198
Intrets <geen.inspiratie824@gmail.com>
2020-12-02 15:42:43 +0100

    removed parsec and using readP from base instead

--- src/AoC2020/Day02.hs
+++ src/AoC2020/Day02.hs
@@ -1,20 +1,21 @@
 module AoC2020.Day02 where
 
-import           Text.ParserCombinators.Parsec
 import           Data.Function
+import           Data.Char
+import           Text.ParserCombinators.ReadP
 
-parser :: (Int -> Int -> Char -> String -> Bool) -> Parser Int
+parser :: (Int -> Int -> Char -> String -> Bool) -> ReadP Int
 parser f = length . filter id <$> many
   (   f
-  <$> (read <$> many digit)
+  <$> (read <$> munch isDigit)
   <*  char '-'
-  <*> (read <$> many digit)
-  <*  spaces
-  <*> letter
+  <*> (read <$> munch isDigit)
+  <*  skipSpaces
+  <*> get
   <*  char ':'
-  <*  spaces
-  <*> many letter
-  <*  newline
+  <*  skipSpaces
+  <*> munch isLetter
+  <*  skipSpaces
   )
 
 main :: IO ()
@@ -24,8 +25,9 @@ main = do
   let part1 a b c = ((&&) <$> (>= a) <*> (<= b)) . length . filter (== c)
   let part2 a b c = ((/=) `on` (== c)) <$> (!! (a - 1)) <*> (!! (b - 1))
 
-  putStr "part one: " >> print (parse (parser part1) "" inputData)
-  putStr "part two: " >> print (parse (parser part2) "" inputData)
-
+  putStr "part one: "
+    >> (print . fst . head $ readP_to_S (parser part1 <* eof) inputData)
+  putStr "part two: "
+    >> (print . fst . head $ readP_to_S (parser part2 <* eof) inputData)
 
 
