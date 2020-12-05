8acd04e53c8d1bc41dd38d1027bc8808d61cb714
Intrets <geen.inspiratie824@gmail.com>
2020-12-05 14:48:25 +0100

    day 5

--- src/AoC2020/Day05.hs
+++ src/AoC2020/Day05.hs
@@ -1,1 +1,19 @@
 module AoC2020.Day05 where
+
+import           Data.Bits
+import           Data.List
+import           Control.Applicative
+
+readBin :: String -> Int
+readBin =
+  sum
+    . zipWith (*) (iterate (`shiftL` 1) 1)
+    . map (length . intersect "BR" . pure)
+    . reverse
+
+main :: IO ()
+main = do
+  inputData <- map readBin . lines <$> readFile "src/AoC2020/Day05.txt"
+
+  putStr "part one: " >> print (maximum inputData)
+  putStr "part two: " >> print ((flip (foldl xor) <*> maximum) inputData)
