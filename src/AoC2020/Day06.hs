215ed5caaaf7593d3cb10ed9d741970b3ea62aed
Intrets <geen.inspiratie824@gmail.com>
2020-12-06 12:36:37 +0100

    day 6

--- src/AoC2020/Day06.hs
+++ src/AoC2020/Day06.hs
@@ -1,1 +1,24 @@
 module AoC2020.Day06 where
+
+import           Text.ParserCombinators.ReadP  as P
+import           Data.Char
+import           Data.List
+import           Control.Applicative
+
+parser :: ReadP [[String]]
+parser = P.many $ lines <$> manyTill get (string "\n\n")
+
+main :: IO ()
+main = do
+  inputData <- fst . head . readP_to_S (parser <* eof) <$> readFile
+    "src/AoC2020/Day06.txt"
+
+  let part1 = length . group . sort . concat
+
+  putStr "part one: " >> print (sum . map part1 $ inputData)
+
+  let part2 = length
+        . liftA2 filter (((==) .) length) (map length . group . sort . concat)
+
+  putStr "part two: " >> print (sum . map part2 $ inputData)
+
