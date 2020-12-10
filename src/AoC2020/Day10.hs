39bb5f1b93f384d0e5913401725d9b29dcf8ccad
Intrets <geen.inspiratie824@gmail.com>
2020-12-10 13:20:41 +0100

    day 10

--- src/AoC2020/Day10.hs
+++ src/AoC2020/Day10.hs
@@ -1,4 +1,31 @@
+{-# LANGUAGE LambdaCase #-}
+
 module AoC2020.Day10 where
 
+import           Control.Monad
+import           Data.List
+
+solve2 :: Integer -> Integer -> Integer -> [Int] -> Integer
+solve2 a b c = a `seq` b `seq` c `seq` \case
+  (1 : ds) -> solve2 b c (a + b + c) ds
+  (2 : ds) -> solve2 c 0 (b + c) ds
+  (3 : ds) -> solve2 0 0 c ds
+  []       -> c
+
 main :: IO ()
-main = putStrLn "day10"
+main = do
+  inputData <-
+    ap (zipWith (flip (-))) tail
+    .   sort
+    .   (0 :)
+    .   ap (flip (:)) ((+ 3) . maximum)
+    .   map read
+    .   lines
+    <$> readFile "src/AoC2020/Day10.txt"
+
+  putStr "part one: "
+  print $ product . map length . group . sort $ inputData
+
+  putStr "part two: "
+  print $ solve2 0 0 1 inputData
+
