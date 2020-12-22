6f1683dabc6eb9c7b5a2dbfc16e6d49c5055f71b
Intrets <geen.inspiratie824@gmail.com>
2020-12-22 15:11:46 +0100

    day 22

--- src/AoC2020/Day22.hs
+++ src/AoC2020/Day22.hs
@@ -6,31 +6,29 @@ import           Control.Monad.State.Strict
 import           Control.Arrow
 import qualified Data.Set                      as S
 
-
-data Queue a = Queue [a] [a] deriving (Show)
+newtype Queue a = Queue [a]
 
 size :: Queue a -> Int
-size (Queue a b) = length a + length b
+size (Queue a) = length a
 
-takeQ n = toQueue . take n . toList
+takeQ n (Queue a) = Queue $ take n a
 
 pop :: Queue a -> (a, Queue a)
-pop (Queue []       a ) = pop (Queue (reverse a) [])
-pop (Queue (a : as) bs) = (a, Queue as bs)
+pop (Queue (a : as)) = (a, Queue as)
 
 push :: [a] -> Queue a -> Queue a
-push a (Queue as bs) = Queue as (a ++ bs)
+push a (Queue as) = Queue (as ++ a)
+--push a (Queue as bs) = Queue as (a ++ bs)
 
 toQueue :: [a] -> Queue a
-toQueue = flip Queue []
+toQueue = Queue
 
 empty :: Queue a -> Bool
-empty (Queue [] []) = True
-empty _             = False
+empty (Queue []) = True
+empty _          = False
 
 toList :: Queue a -> [a]
-toList (Queue a b) = a ++ reverse b
-
+toList (Queue a) = a
 
 parseDeck :: ReadP [Int]
 parseDeck = sepBy (read <$> munch1 isDigit) (char '\n')
@@ -47,11 +45,9 @@ parser = do
   skipSpaces
   pure (deck1, deck2)
 
-
-
 playGame :: Queue Int -> Queue Int -> (Int, [Int])
-playGame (Queue [] []) p2            = (2, toList p2)
-playGame p1            (Queue [] []) = (1, toList p1)
+playGame (Queue []) p2         = (2, toList p2)
+playGame p1         (Queue []) = (1, toList p1)
 playGame p1 p2 | d1 < d2   = playGame s1 (push [d1, d2] s2)
                | d1 > d2   = playGame (push [d2, d1] s1) s2
                | otherwise = undefined
@@ -60,8 +56,8 @@ playGame p1 p2 | d1 < d2   = playGame s1 (push [d1, d2] s2)
   (d2, s2) = pop p2
 
 playGame2 :: S.Set ([Int], [Int]) -> (Queue Int, Queue Int) -> (Int, Queue Int)
-playGame2 history (p1         , Queue [] []) = (1, p1)
-playGame2 history (Queue [] [], p2         ) = (2, p2)
+playGame2 history (p1      , Queue []) = (1, p1)
+playGame2 history (Queue [], p2      ) = (2, p2)
 playGame2 history (p1, p2)
   | (toList p1, toList p2) `S.member` history
   = (1, p1)
@@ -91,7 +87,6 @@ main = do
   print $ sum . zipWith (*) (reverse deck) $ [1 ..]
 
   let (winner2, deck2) = playGame2 S.empty (toQueue player1, toQueue player2)
-
 
   putStr "part two: "
   print $ sum . zipWith (*) (reverse . toList $ deck2) $ [1 ..]
