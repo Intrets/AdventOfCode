a786fa78550e2d9fd3a968695feff08b7ac73ab3
Intrets <geen.inspiratie824@gmail.com>
2020-12-25 12:47:57 +0100

    day 25

--- src/AoC2020/Day25.hs
+++ src/AoC2020/Day25.hs
@@ -1,4 +1,17 @@
 module AoC2020.Day25 where
 
+import           Data.List
+
+rng = ((`mod` 20201227) .) . (*)
+
+findLoopSize x = succ . length . takeWhile (/= x) . iterate (rng 7)
+
+loop n subject = (!! pred n) . iterate (rng subject)
+
 main :: IO ()
-main = putStrLn "day25"
+main = do
+  [card, door] <- map (read :: String -> Int) . lines <$> readFile
+    "src/AoC2020/Day25.txt"
+
+  print $ loop (findLoopSize card 7) door door
+
