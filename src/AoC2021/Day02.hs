c4000e6cd754bdad326846eb796123d87e7abe38
Intrets <geen.inspiratie824@gmail.com>
2021-12-02 12:38:35 +0100

    day2

--- src/AoC2021/Day02.hs
+++ src/AoC2021/Day02.hs
@@ -1,9 +1,29 @@
+{-# LANGUAGE ViewPatterns #-}
 module AoC2021.Day02 where
 
 inputFile = readFile "src/AoC2021/Day02.txt"
 
+part1 (x, y) ["forward", read -> value] = (x + value, y)
+part1 (x, y) ["up"     , read -> value] = (x, y - value)
+part1 (x, y) ["down"   , read -> value] = (x, y + value)
+
+part2 (x, y, aim) ["forward", read -> value] =
+  (x + value, y + aim * value, aim)
+part2 (x, y, aim) ["up"  , read -> value] = (x, y, aim - value)
+part2 (x, y, aim) ["down", read -> value] = (x, y, aim + value)
+
 main :: IO ()
 main = do
-  putStr "part 1: " >> print ""
-  putStr "part 2: " >> print ""
+  part1Answer <-
+    uncurry (*) . foldl part1 (0, 0) . map words . lines <$> inputFile
+
+  part2Answer <-
+    (\(x, y, _) -> x * y)
+    .   foldl part2 (0, 0, 0)
+    .   map words
+    .   lines
+    <$> inputFile
+
+  putStr "part 1: " >> print part1Answer
+  putStr "part 2: " >> print part2Answer
 
