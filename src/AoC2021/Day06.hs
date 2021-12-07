5407b0600eaaa046e0a5476b3d0e915e8332f331
Intrets <geen.inspiratie824@gmail.com>
2021-12-07 01:09:38 +0100

    day 6 again

--- src/AoC2021/Day06.hs
+++ src/AoC2021/Day06.hs
@@ -5,11 +5,13 @@ import           Data.List
 import           Control.Arrow
 import           Data.Bool
 import           Data.Function
+import           Control.Monad
 
 inputFile = readFile "src/AoC2021/Day06.txt"
 
 step (count, 0  ) = [(count, 6), (count, 8)]
 step (count, age) = [(count, age - 1)]
+
 
 main :: IO ()
 main = do
@@ -33,6 +35,28 @@ main = do
               )
           $ fishes
 
-  putStr "part 1: " >> print (answer 80)
-  putStr "part 2: " >> print (answer 256)
+  fishes2 <-
+    map (pred . length)
+    .   group
+    .   sort
+    .   (++) (enumFromTo 0 6)
+    .   map read
+    .   splitOn ","
+    <$> inputFile
+
+  let answer2 day =
+        sum
+          . concat
+          . (!! day)
+          . iterate
+              (ap ((map tail .) . flip (map . flip (++) . pure))
+                  (sum . head . transpose)
+              )
+          . (: [replicate 9 0])
+          $ fishes2
+
+  putStr "part 1: " >> print (answer2 80)
+  putStr "part 2: " >> print (answer2 256)
+
+
 
