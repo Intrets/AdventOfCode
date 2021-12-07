39cf73abfab6c0efd4b7b4f132ba8e87de4b2080
Intrets <geen.inspiratie824@gmail.com>
2021-12-07 20:23:31 +0100

    day 7

--- src/AoC2021/Day07.hs
+++ src/AoC2021/Day07.hs
@@ -1,9 +1,33 @@
 module AoC2021.Day07 where
 
+import           Data.List.Split
+import           Data.List
+import           Data.Ord
+import           Control.Monad
+
 inputFile = readFile "src/AoC2021/Day07.txt"
 
 main :: IO ()
 main = do
-  putStr "part 1: " >> print ""
-  putStr "part 2: " >> print ""
+  crabs <- map read . splitOn "," <$> inputFile
+
+  let part1 =
+        minimum
+          . (   zipWith ((sum .) . map . ((abs .) . (-)))
+            <$> (enumFromTo 0 . maximum)
+            <*> repeat
+            )
+
+  let
+    part2 =
+      minimum
+        . (   zipWith
+              ((sum .) . map . (((`div` 2) .) . (ap (*) succ .) . (abs .) . (-))
+              )
+          <$> (enumFromTo 0 . maximum)
+          <*> repeat
+          )
+
+  putStr "part 1: " >> print (part1 crabs)
+  putStr "part 2: " >> print (part2 crabs)
 
