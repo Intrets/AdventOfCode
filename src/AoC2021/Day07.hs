cc15e180eac2d6b5ba0c0e4888c180a1687ce3d7
Intrets <geen.inspiratie824@gmail.com>
2021-12-07 20:46:32 +0100

    day 7

--- src/AoC2021/Day07.hs
+++ src/AoC2021/Day07.hs
@@ -7,26 +7,26 @@ import           Control.Monad
 
 inputFile = readFile "src/AoC2021/Day07.txt"
 
+(.:) = (.) . (.)
+infixr 8 .:
+
 main :: IO ()
 main = do
   crabs <- map read . splitOn "," <$> inputFile
 
   let part1 =
         minimum
-          . (   zipWith ((sum .) . map . ((abs .) . (-)))
+          . (   zipWith (sum .: map . (abs .: (-)))
             <$> (enumFromTo 0 . maximum)
             <*> repeat
             )
 
-  let
-    part2 =
-      minimum
-        . (   zipWith
-              ((sum .) . map . (((`div` 2) .) . (ap (*) succ .) . (abs .) . (-))
-              )
-          <$> (enumFromTo 0 . maximum)
-          <*> repeat
-          )
+  let part2 =
+        minimum
+          . (   zipWith (sum .: map . ((`div` 2) .: ap (*) succ .: abs .: (-)))
+            <$> (enumFromTo 0 . maximum)
+            <*> repeat
+            )
 
   putStr "part 1: " >> print (part1 crabs)
   putStr "part 2: " >> print (part2 crabs)
