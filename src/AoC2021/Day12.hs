0f8d025c8ef445f219c89cb47ebe28014b829220
Intrets <geen.inspiratie824@gmail.com>
2021-12-12 14:40:24 +0100

    day 12 part 1

--- src/AoC2021/Day12.hs
+++ src/AoC2021/Day12.hs
@@ -1,9 +1,80 @@
 module AoC2021.Day12 where
 
+import           Data.List.Split
+import qualified Data.Map                      as M
+import           Control.Monad.State
+import           Control.Monad
+import           Control.Applicative
+import qualified Data.Set                      as S
+import           Data.Char
+import           Data.List
+import           Control.Arrow
+import           Data.Bifunctor                 ( bimap )
+
 inputFile = readFile "src/AoC2021/Day12.txt"
+
+data Cave = Small String | Large String deriving (Ord, Eq, Show, Read)
+
+start = Small "start"
+end = Small "end"
+
+isSmall (Small _) = True
+isSmall _         = False
+
+type S a = State ([Cave], S.Set Cave) a
+emptyS = ([], S.empty)
+
+isVisited :: Cave -> S Bool
+isVisited cave = (isSmall cave &&) <$> gets (S.member cave . snd)
+
+visit :: Cave -> S ()
+visit cave = modify $ bimap (cave :) (S.insert cave)
+
+pop :: S ()
+pop = do
+  top <- gets (head . fst)
+  modify $ first tail
+  modify $ second (S.delete top)
+
+
+solve :: (Cave -> [Cave]) -> State ([Cave], S.Set Cave) Int
+solve getNeighbours = do
+
+  let search (Small "end") = return 1
+      search cave          = do
+        visited <- isVisited cave
+        if visited
+          then return 0
+          else do
+            visit cave
+            let neighbours = getNeighbours cave
+            res <- sum <$> mapM search neighbours
+            pop
+            return res
+
+  search start
+
 
 main :: IO ()
 main = do
+  graph <-
+    (M.!)
+    .   M.unionsWith (++)
+    .   map (M.fromList . pure)
+    .   concatMap
+          ( (\[a, b] -> [(a, [b]), (b, [a])])
+          . map (\s -> if all isUpper s then Large s else Small s)
+          . splitOn "-"
+          )
+    .   lines
+    <$> inputFile
+
+  let part1 = runState (solve graph) emptyS
+
+  print part1
+
+  -- print graph
+
   putStr "part 1: " >> print ""
   putStr "part 2: " >> print ""
 
