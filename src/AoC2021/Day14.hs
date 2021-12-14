2d42255ce02e93cd1ac2b8989c8d75cea098585b
Intrets <geen.inspiratie824@gmail.com>
2021-12-14 13:59:36 +0100

    day 14

--- src/AoC2021/Day14.hs
+++ src/AoC2021/Day14.hs
@@ -1,17 +1,18 @@
-{-# LANGUAGE TupleSections #-}
+
 module AoC2021.Day14 where
 
 import           Text.ParserCombinators.ReadP
 import           Data.List
 import           Data.Char
 import           Control.Monad
-import qualified Data.Map                      as M
+import qualified Data.Map.Strict               as M
 import           Control.Monad.State
 import           Data.Function
 import           Control.Arrow
 
 inputFile = readFile "src/AoC2021/Day14.txt"
 
+parse :: ReadP (String, [((Char, Char), Char)])
 parse = do
   start <- munch isAlpha
   skipSpaces
@@ -24,17 +25,15 @@ parse = do
     return ((a, b), to)
   return (start, rules)
 
-step
-  :: ((Char, Char) -> Char)
-  -> [((Char, Char), Integer)]
-  -> [((Char, Char), Integer)]
-step mapping =
-  map (foldl1 (\((a, b), n) (_, m) -> ((a, b), n + m)))
-    . groupBy ((==) `on` fst)
-    . sortOn fst
-    . concatMap
-        (\((a, b), n) -> [((a, mapping (a, b)), n), ((mapping (a, b), b), n)])
-
+solve mapping steps start =
+  flip evalState (foldr (flip (M.insertWith (+)) 1) M.empty start) $ do
+    replicateM_ steps $ do
+      pairs <- gets M.toList
+      put M.empty
+      forM_ pairs $ \((a, b), n) -> do
+        modify (M.insertWith (+) (a, mapping (a, b)) n)
+        modify (M.insertWith (+) (mapping (a, b), b) n)
+    gets $ map (first fst) . M.toList
 
 
 main :: IO ()
@@ -43,18 +42,16 @@ main = do
   let (start, rules) = fst . head . readP_to_S (parse <* eof) $ input
   let mapping        = (M.!) . M.fromList $ rules
 
-  let pairs          = map (, 1) . ap zip tail $ start
-  let counts n =
-        ((-) <$> maximum <*> minimum)
-          . map (snd . foldl1 (\(a, n) (_, m) -> (a, n + m)))
-          . groupBy ((==) `on` fst)
-          . sortOn fst
-          . (++ [(head start, 1), (last start, 1)])
-          . map (first fst)
-          . (!! n)
-          . iterate (step mapping)
-          $ pairs
+  let pairs          = ap zip tail start
 
-  putStr "part 1: " >> print (counts 10)
-  putStr "part 2: " >> print (counts 40)
+  let solve2 n =
+        ((-) <$> maximum <*> minimum)
+          .  map (sum . map snd)
+          .  groupBy ((==) `on` fst)
+          .  sortOn fst
+          $  [(head start, 1), (last start, 1)]
+          ++ solve mapping n pairs
+
+  putStr "part 1: " >> print (solve2 10)
+  putStr "part 2: " >> print (solve2 40)
 
