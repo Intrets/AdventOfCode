99cf94982e21fe70308ebd3986ee59a50da4298d
Intrets <geen.inspiratie824@gmail.com>
2021-12-20 15:35:43 +0100

    day 20

--- src/AoC2021/Day20.hs
+++ src/AoC2021/Day20.hs
@@ -1,9 +1,37 @@
+{-# LANGUAGE ViewPatterns #-}
 module AoC2021.Day20 where
+
+import           Data.List
+import qualified Data.Vector                   as V
+import           Data.Bool
+import           Data.List.Split
 
 inputFile = readFile "src/AoC2021/Day20.txt"
 
+getIndex :: String -> Int
+getIndex = foldl1 ((+) . (2 *)) . map (bool 0 1 . (== '#'))
+
+
 main :: IO ()
 main = do
-  putStr "part 1: " >> print ""
-  putStr "part 2: " >> print ""
+  [concat -> enhanceList, seed] <- splitOn [""] . lines <$> inputFile
+
+  let enhance = (V.!) $ V.fromList enhanceList
+
+  let pad n = foldl1 (.) $ replicate 2 $ transpose . map
+        ((replicate n '.' ++) . (++ replicate n '.'))
+
+  let part1Board = pad 4 seed
+
+  let group3 = map (zipWith3 (\a b c -> [a, b, c]) <*> tail <*> tail . tail)
+
+  let groupNeighbours =
+        map (map concat) . transpose . group3 . transpose . group3
+
+  let step = map (map (enhance . getIndex)) . groupNeighbours
+
+  let solve n = length . filter (== '#') . concat . (!! n) . iterate step . pad (n * 2) $ seed
+
+  putStr "part 1: " >> print (solve 2)
+  putStr "part 2: " >> print (solve 50)
 
